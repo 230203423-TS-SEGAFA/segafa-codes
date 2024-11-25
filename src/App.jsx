1d121f89a603530f3fcbb5ef8e42cdd8857fb644
230203423-TS-SEGAFA <230203423@tut4life.ac.za>
2024-11-25 09:30:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './component/Dashboard';
+import AddCard from './component/AddCard';
 
 function App() {
+  const user = {
+    profilePicture: 'path/to/profile-picture.jpg',
+    name: 'Thulaganyo',
+    email: 'john.doe@example.com',
+  };
+
+  const handleLogout = () => {
+    console.log('User logged out');
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/*" element={<Dashboard user={user} onLogout={handleLogout} />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
